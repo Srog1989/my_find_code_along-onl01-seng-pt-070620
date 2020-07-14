17019561322691d58d78bcbc00ce794fded7f87c
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-14 18:35:58 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,7 +2,9 @@ require 'pry'
 
 def my_find(collection)
   i = 0
-  while i< collection.length return collection[i]
+  while i< collection.length
+  return collection[i]
+  yield(collection[i]
     i = i +1
   end
 end
